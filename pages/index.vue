3dca39fd9266ff448d49442ddaaec3e3da71b8b5
Ryuze <alfianyunianto74@gmail.com>
2021-10-26 12:19:23 +0700

    update vuex data

--- pages/index.vue
+++ pages/index.vue
@@ -59,6 +59,7 @@
 <script>
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import { mapState } from 'vuex'
 
 export default {
   data() {
@@ -67,100 +68,18 @@ export default {
         name: 'Alfian Yunianto Suseno',
         photo: require('~/assets/profile.jpg'),
       },
-      education: [
-        {
-          school: 'Universitas Singaperbangsa Karawang',
-          degree: 'Sarjana 1',
-          major: 'Teknik Informatika',
-          gpa: '3,46',
-        },
-        {
-          school: 'SMK Negeri 1 Cikarang Selatan',
-          degree: 'SMK',
-          major: 'Teknik Komputer Jaringan',
-          gpa: '78,56',
-        },
-      ],
-      experience: [
-        {
-          company: 'KPP Pratama Karawang',
-          job: 'Internship IT Support',
-          time: '1 Bulan',
-        },
-        {
-          company: 'Freelance',
-          job: 'Fullstack Web Developer',
-          time: '3 Bulan',
-        },
-      ],
-      skillPr: [
-        {
-          name: 'PHP',
-          image: require('~/assets/icon/php.svg'),
-        },
-        {
-          name: 'Javascript',
-          image: require('~/assets/icon/javascript.svg'),
-        },
-        {
-          name: 'Python',
-          image: require('~/assets/icon/python.svg'),
-        },
-        {
-          name: 'HTML',
-          image: require('~/assets/icon/html.svg'),
-        },
-      ],
-      skillMisc: [
-        {
-          name: 'Mikrotik',
-          image: require('~/assets/icon/mikrotik.svg'),
-        },
-        {
-          name: 'Perbaikan Komputer',
-          image: require('~/assets/icon/repair.svg'),
-        },
-      ],
-      portofolio: [
-        {
-          name: 'Simple Short Link',
-          link: 'https://shrt.alfian.live',
-          image: require('~/assets/portofolio/sslink.png'),
-          desc: 'Aplikasi ini berfungsi sebagai pemendek link yang cukup panjang, cara kerjanya adalah menyimpan link pada database yang kemudian akan menghasilkan random string sebagai kunci dari link tersebut. Ketika kunci digunakan maka akan redirect menuju link yang telah disimpan.',
-        },
-      ],
-      cert: [
-        {
-          name: 'MTCNA',
-          id: '2110NA5887',
-          image: require('~/assets/certificate/mtcna.png')
-        },
-      ],
-      contact: [
-        {
-          name: 'phone',
-          id: '+6282213515619',
-          icon: require('~/assets/icon/phone-white.svg')
-        },
-        {
-          name: 'github',
-          link: 'https://github.com/ryuze',
-          id: 'Alfian Yunianto Suseno',
-          icon: require('~/assets/icon/github-white.svg')
-        },
-        {
-          name: 'gmail',
-          id: 'Alfianyunianto74@gmail.com',
-          icon: require('~/assets/icon/gmail.svg')
-        },
-        {
-          name: 'linkedin',
-          link: 'https://www.linkedin.com/in/alfian-yunianto',
-          id: 'Alfian Yunianto Suseno',
-          icon: require('~/assets/icon/linkedin.svg')
-        },
-      ]
     }
+  },
+  computed: {
+    ...mapState([
+      'education',
+      'experience',
+      'skillPr',
+      'skillMisc',
+      'portofolio',
+      'cert',
+      'contact'
+    ])
   },
   mounted() {
     window.VANTA.WAVES({
